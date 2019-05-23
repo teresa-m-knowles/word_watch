a5cca57a9c59ec5d2f716e4325e12321e0c1d8ec
Teresa Knowles <teresa07moreno@gmail.com>
2019-05-23 11:06:31 -0600

    Use getTopWord and sendWord in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 import $ from 'jquery'
+let api = require('./modules/functions.js')
+
+const fetch = require('node-fetch');
+let topWord;
+let frequency;
 
 $(document).ready(() => {
-  // have fun!
+
+  api.getTopWord()
+    .then(response => {
+      topWord = Object.keys(response.word)[0]
+      frequency = response.word[topWord]
+      $('.word-count').text(`${topWord.toUpperCase()} Count: ${frequency}`)
+    });
+
+  $('#breakdown').click(function() {
+    var text = $('#input').val();
+    var words = text.match(/\b(\w+)\b/g)
+    console.log(words)
+    let i;
+    for (i=0; i < words.length; i++) {
+      api.sendWord(words[i])
+        .then(response => {
+          console.log(response)
+        })
+    }
+  });
 })
